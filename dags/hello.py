f72770a00e7f9e5cd96637c495f7b071c4f40bc0
MouadBH <mouad123b@gmail.com>
2024-02-24 20:03:47 +0100

    feat: update schedule interval

--- dags/hello.py
+++ dags/hello.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 with DAG(
     dag_id="hello_world",
     start_date=datetime(2024, 1, 25),
-    schedule=timedelta(minutes=1),
+    schedule=timedelta(hours=1),
 ):
     task1 = BashOperator(
         task_id = 'hello_world',
