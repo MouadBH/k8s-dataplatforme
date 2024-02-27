f2f7a2b98b2b92c31ed0aa39c34a9e9cd626b29f
MouadBH <mouad123b@gmail.com>
2024-02-27 23:49:10 +0100

    feat: update dag

--- dags/hello_dag.py
+++ dags/hello_dag.py
@@ -1,12 +1,27 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
 
+
+default_args = {
+    'owner': 'mouadbh',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'max_active_runs': 1,
+    'retries': 3
+}
 
 dag = DAG(
     dag_id="hello_world",
-    start_date=datetime(2024, 1, 25),
-    schedule=timedelta(days=1),
+    start_date=days_ago(1),
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    description='testing airflow dags.',
+    tags=['example']
 )
 task1 = BashOperator(
     task_id = 'hello_world',
