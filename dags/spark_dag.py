936289ed641ff8bfeda84649370eb939ddae2c13
MouadBH <mouad123b@gmail.com>
2024-02-27 01:26:13 +0100

    feat: spark dag example poc

--- dags/spark_dag.py
+++ dags/spark_dag.py
@@ -27,7 +27,7 @@ with DAG(
     submit = SparkKubernetesOperator(
         task_id='spark_transform_data',
         namespace='spark-operator',
-        application_file='/kubernetes/spark-pi.yaml',
+        application_file='/k8s/spark-pi.yaml',
         kubernetes_conn_id='kubernetes_default',
         do_xcom_push=True,
     )
