17396d6af8c7c2832ff98583e993e0e500bf195a
MouadBH <mouad123b@gmail.com>
2024-02-27 01:44:24 +0100

    feat: fix k8s namespace

--- dags/spark_dag.py
+++ dags/spark_dag.py
@@ -22,11 +22,12 @@ with DAG(
     start_date=days_ago(1),
     default_args=default_args,
     schedule_interval=timedelta(days=1),
+    description='submit spark-pi as sparkApplication on kubernetes',
     tags=['example']
 ):
     submit = SparkKubernetesOperator(
         task_id='spark_transform_data',
-        namespace='spark-operator',
+        namespace='spark-apps',
         application_file='/k8s/spark-pi.yaml',
         kubernetes_conn_id='kubernetes_default',
         do_xcom_push=True,
